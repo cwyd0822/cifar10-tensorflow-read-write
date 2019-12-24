4179a97d76c480a839ebcead7da6a02166946196
cwyd0822 <cwyd0822@qq.com>
2019-12-24 15:52:58 +0800

    添加注释

--- reader_cifar10-2.py
+++ reader_cifar10-2.py
@@ -1,15 +1,28 @@
+"""
+读取文件数据
+"""
+
 import tensorflow as tf
 
+# 我们放了3个文件在相应的位置
 filename = ['data/A.csv', 'data/B.csv', 'data/C.csv']
 
+# 将文件的路径作为参数传入函数
+# 输出是文件队列，无法直接获取文件的值
 file_queue = tf.train.string_input_producer(filename,
                                             shuffle=True,
                                             num_epochs=2)
+
+# 文件读取器
 reader = tf.WholeFileReader()
+# key：文件名 value:文件值
 key, value = reader.read(file_queue)
 
 with tf.Session() as sess:
+    # 对局部变量进行赋值
     sess.run(tf.local_variables_initializer())
-    tf.train.start_queue_runners(sess=sess)
-    for i in range(10):
+    tf.train.start_queue_runners(sess=sess)  # 定义文件队列填充的线程
+    for i in range(6):  # 文件数量3 * 2epochs
         print(sess.run([key, value]))
+
+
